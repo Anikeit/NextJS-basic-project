56ecf68e36b034cad7579ae1278f41599a40f97a
anikeit <18dcs017@lnmiit.ac.in>
2022-06-05 12:41:40 +0530

    MR Purpose

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../comp/header'
 import Footer from '../comp/footer'
